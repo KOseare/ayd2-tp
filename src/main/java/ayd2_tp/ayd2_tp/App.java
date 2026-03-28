35c2a4472364019f5b9092bb95c24a24b7cf3657
Dario Funes <kdfunes@outlook.com>
2026-03-27 23:49:40 -0300

    First commit

---
+++ src/main/java/ayd2_tp/ayd2_tp/App.java
@@ -0,0 +1,13 @@
+package ayd2_tp.ayd2_tp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
