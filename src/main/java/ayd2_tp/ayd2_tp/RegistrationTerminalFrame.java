aee177428e979831bf12ac4c33fd657d84794fd5
Dario Funes <kdfunes@outlook.com>
2026-03-30 09:03:43 -0300

    Coneccion terminal de registro directo con monitor

--- src/main/java/ayd2_tp/ayd2_tp/RegistrationTerminalFrame.java
+++ src/main/java/ayd2_tp/ayd2_tp/RegistrationTerminalFrame.java
@@ -1,7 +1,14 @@
 package ayd2_tp.ayd2_tp;
 
 import java.awt.BorderLayout;
+
 import java.awt.GridLayout;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -11,9 +18,32 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
+
+
 public class RegistrationTerminalFrame extends JFrame {
 
     private static final long serialVersionUID = 1L;
+    
+    JTextField documentField;
+    
+    public void sendData () {
+    	new Thread() {
+    		public void run () {
+    			try {
+    	    		// Agregar threads
+    	    		Socket socket = new Socket("127.0.0.1", 3001);
+    	    		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    	    		BufferedReader in = new BufferedReader(new
+    	    		InputStreamReader(socket.getInputStream()));
+    	    		out.println(documentField.getText());
+    	    		out.close();
+    	    		socket.close();
+    			} catch (Exception e) {
+    	    		e.printStackTrace();
+    			}
+    		}
+    	}.start();
+    }
 
     public RegistrationTerminalFrame() {
         setTitle("Terminal de Registro");
@@ -24,7 +54,7 @@ public class RegistrationTerminalFrame extends JFrame {
         JLabel title = new JLabel("Ingrese su número de documento");
         JLabel hint = new JLabel("Ejemplo mostrado: 99.876.543");
 
-        JTextField documentField = new JTextField("99.876.543");
+        documentField = new JTextField("99.876.543");
         documentField.setColumns(18);
 
         JButton joinButton = new JButton("Unirse a la lista de espera");
@@ -38,5 +68,9 @@ public class RegistrationTerminalFrame extends JFrame {
 
         setLayout(new BorderLayout());
         add(form, BorderLayout.CENTER);
+        
+        joinButton.addActionListener(e -> {
+            sendData();
+        });
     }
 }
